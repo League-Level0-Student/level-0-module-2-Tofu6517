b8ea7460792ca78541090d9f7fe66c0542a05638
league <league@imac-20.attlocal.net>
2022-11-19 16:28:57 -0800

    fix

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,12 +5,20 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
+		public static void main(String[] args) {
+			
 		
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+			String heightToRide = JOptionPane.showInputDialog("whats your hight");
+			int height = Integer.parseInt(heightToRide);
 		// Uncomment the line below...
 			//if( heightToRide > 42 ) { }
-		
+		if( height < 42 ){ 
+			JOptionPane.showMessageDialog(null,"You can ride the coaster with someone else");
+		}
+		if(height > 42) { 
+			JOptionPane.showMessageDialog(null,"you are tall enough to ride the coaster alone");
+		}
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
 		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
@@ -22,4 +30,5 @@ public class RollerCoaster {
 		// "You can ride the coaster with someone else!" 
 	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-} 
+		}
+		} 
---
+++ src/_03_string_conversion/_4_voting_booth/aaaaaaaaaaa.java
@@ -0,0 +1,25 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class aaaaaaaaaaa {
+	public static void main(String[] args) {
+			
+		
+		
+			String age1 = JOptionPane.showInputDialog("how old are you");
+			int age = Integer.parseInt(age1);
+		
+		if( age < 18 ){ 
+			JOptionPane.showMessageDialog(null,"no one cares what you think");
+		}
+	if(age > 18) {
+		JOptionPane.showMessageDialog(null,"who should be the next president");
+		
+	}
+
+		}
+		
+		}
+	
+
